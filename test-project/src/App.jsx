2cf12ed73c0b324d83d6115e72fa1951fe576451
TRAN-cd <ngockiet.tran11@gmail.com>
2026-03-21 12:18:27 +0900

    feature/task04

--- test-project/src/App.jsx
+++ test-project/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Archive from './components/Archive/Archive';
 import Post from './components/PostDetail/Post';
 import { posts } from './data/posts';
+import ContactForm from './components/Contact/ContactForm';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Archive />} />
         <Route path='/post/:id' element={<Post src={posts} />} />
+        <Route path='/contact' element={<ContactForm />} />
       </Routes>
     </>
   )
